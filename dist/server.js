9eb6c134fb040703ade6c3d95364b23223db5346
Kelson Victor <98909425+K3lOli@users.noreply.github.com>
2023-10-26 19:39:13 -0300

    branch public

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = require("./http");
-require("./websocket");
-const PORT = 3000;
-http_1.serverHttp.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT);
-});
--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import { serverHttp, io } from "./http";
-import "./websocket"
-
-const PORT = 3000;
-
-serverHttp.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT)
-})
