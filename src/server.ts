ba7a7a15fc381df8570f7b3bb5ada33f187340b7
Kelson Victor <98909425+K3lOli@users.noreply.github.com>
2023-10-26 20:56:22 -0300

    configurando process.env.PORT

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { serverHttp, io } from "./http";
 import "./websocket"
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
-serverHttp.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT)
+serverHttp.listen(port, () => {
+    console.log('Server is listening on port ' + port)
 })
